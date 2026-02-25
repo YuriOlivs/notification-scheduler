a669f8c1b62ae42d1fba8b109975bbd8a077f252
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-02-24 23:29:23 -0300

    feat: add service and repository implementation

--- src/main/java/com/yuriolivs/notification_scheduler/repository/ScheduleRepository.java
+++ src/main/java/com/yuriolivs/notification_scheduler/repository/ScheduleRepository.java
@@ -3,9 +3,13 @@ package com.yuriolivs.notification_scheduler.repository;
 import com.yuriolivs.notification_scheduler.domain.schedule.entities.ScheduledNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ScheduleRepository extends JpaRepository<ScheduledNotification, UUID> {
     Optional<ScheduledNotification> findById(UUID id);
+    List<ScheduledNotification> findAll();
+    List<ScheduledNotification> findAllByDate(LocalDate date);
 }
--- src/main/java/com/yuriolivs/notification_scheduler/service/SchedulerService.java
+++ src/main/java/com/yuriolivs/notification_scheduler/service/SchedulerService.java
@@ -1,10 +1,18 @@
 package com.yuriolivs.notification_scheduler.service;
 
+import com.yuriolivs.notification.shared.exceptions.http.HttpNotFoundException;
+import com.yuriolivs.notification_scheduler.domain.schedule.dto.ScheduleRequestDTO;
+import com.yuriolivs.notification_scheduler.domain.schedule.entities.ScheduledNotification;
 import com.yuriolivs.notification_scheduler.domain.schedule.interfaces.SchedulerServiceInterface;
 import com.yuriolivs.notification_scheduler.repository.ScheduleRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -13,13 +21,16 @@ public class SchedulerService implements SchedulerServiceInterface {
     private final ScheduleRepository repo;
 
     @Override
-    public void checkScheduleStatus() {
+    public ScheduledNotification checkScheduleStatus(UUID id) {
+        Optional<ScheduledNotification> existing = repo.findById(id);
+        if (existing.isEmpty()) throw new HttpNotFoundException("Schedule not found.");
 
+        return existing.get();
     }
 
     @Override
-    public void scheduleMessage() {
-
+    public ScheduledNotification scheduleMessage(ScheduleRequestDTO dto) {
+        return null;
     }
 
     @Override
@@ -28,12 +39,22 @@ public class SchedulerService implements SchedulerServiceInterface {
     }
 
     @Override
-    public void returnAllScheduledMessages() {
-
+    public List<ScheduledNotification> findAllScheduledMessages() {
+        return repo.findAll();
     }
 
     @Override
-    public void cancelSchedule() {
+    public List<ScheduledNotification> findAllScheduledMessagesByDate(LocalDate date) {
+        return repo.findAllByDate(date);
+    }
 
+    @Override
+    public ScheduledNotification cancelSchedule(UUID id) {
+        Optional<ScheduledNotification> existing = repo.findById(id);
+        if (existing.isEmpty()) throw new HttpNotFoundException("Schedule not found.");
+
+        ScheduledNotification notification = existing.get();
+        notification.setIsActive(false);
+        return repo.save(notification);
     }
 }
