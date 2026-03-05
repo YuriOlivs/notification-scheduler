763a8fc8e9b033a13afde3b34eab22c6debf7a8a
Yuri Oliveira <yuri.os.2004@outlook.com>
2026-03-05 20:34:28 -0300

    feat: add validation exception handler

--- src/main/java/com/yuriolivs/notification_scheduler/config/GlobalExceptionHandler.java
+++ src/main/java/com/yuriolivs/notification_scheduler/config/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package com.yuriolivs.notification_scheduler.config;
 
 import com.yuriolivs.notification.shared.exceptions.ErrorResponse;
 import com.yuriolivs.notification.shared.exceptions.http.HttpException;
+import com.yuriolivs.notification.shared.exceptions.http.HttpStatus;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +22,26 @@ public class GlobalExceptionHandler {
         ErrorResponse err = new ErrorResponse(
                 ex.getStatus().getCode(),
                 ex.getMessage(),
+                request.getRequestURI(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(err.getStatusCode())
+                .body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request
+    ) {
+        String message = ex.getFieldError().getDefaultMessage() != null
+                ? ex.getFieldError().getDefaultMessage() : "Bad Request";
+
+        ErrorResponse err = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.getCode(),
+                message,
                 request.getRequestURI(),
                 LocalDateTime.now()
         );
